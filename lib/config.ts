692a09aa9c7a0d1df1faa4c17ab18400980da34d
Ashok Kumar <ashokgupta2678@gmail.com>
2025-10-14 16:29:32 +0530

    changed sku data to match the real data
    Signed-off-by: Ashok Kumar <ashokgupta2678@gmail.com>

--- lib/config.ts
+++ lib/config.ts
@@ -3,4 +3,4 @@
  * Adjust this value to make the entire planogram larger or smaller on the screen.
  * Example: A value of 0.2 means a 1000mm wide object will be rendered as 200px wide.
  */
-export const PIXELS_PER_MM = 0.4;
+export const PIXELS_PER_MM = 0.5;
