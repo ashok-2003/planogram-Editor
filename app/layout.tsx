023f7f83801426b1eb6052a05f60efcd176c983b
Ashok Kumar <ashokgupta2678@gmail.com>
2025-10-26 15:12:48 +0530

    added the toaster to the bottom right
    Signed-off-by: Ashok Kumar <ashokgupta2678@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster 
-          position="top-right"
+          position="bottom-right"
           toastOptions={{
             duration: 3000,
             style: {
